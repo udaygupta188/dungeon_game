9676dea63660a0e1ed79419241804f7adc3a0c79
Uday Gupta <62444103+udaygupta188@users.noreply.github.com>
2024-05-05 03:25:38 +0530

    dairy inventory is in rogress

--- dbConn.php
+++ dbConn.php
@@ -9,7 +9,7 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Success!";
+    // echo "Success!";
 } catch(PDOException $e) {
     echo "Database couldn't be connected: " . $e->getMessage();
 }
