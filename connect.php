40b47f4001e91e32217e5ba23876b616ea0ac764
Uday Gupta <62444103+udaygupta188@users.noreply.github.com>
2024-05-06 03:10:00 +0530

    db user data

--- connect.php
+++ connect.php
@@ -1,18 +1,9 @@
 
 <?php
-// $host = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "api_fiverr";
-
-// try {
-//     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-//     // set the PDO error mode to exception
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     // echo "Success!";
-// } catch(PDOException $e) {
-//     echo "Database couldn't be connected: " . $e->getMessage();
-// }
+// $host="localhost";
+// $username="u314280334_test";
+// $password="Vanags11!";
+// $dbname="u314280334_test";
 
 
 $host="localhost";
